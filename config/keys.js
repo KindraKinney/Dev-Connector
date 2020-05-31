a1e26d640b751ab5cd6e6fa696ff9d711a28afe4
Kindra Kinney <kindrakinney@gmail.com>
2020-05-31 10:09:46 -0700

    added login validation, user model, profile model

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 
-    'mongodb+srv://kinneyk12:Parakeet92@devconnect-a3uw3.mongodb.net/test?retryWrites=true&w=majority'
-}
+    'mongodb+srv://kinneyk12:Parakeet-92@devconnect-a3uw3.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+};
